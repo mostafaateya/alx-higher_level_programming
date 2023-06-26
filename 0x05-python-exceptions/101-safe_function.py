7b0ecd39d7ff2e1b95d98c1332dc2a7b2a6df7b5
mostafaateya <mostafaateya@gmail.com>
2023-06-26 16:32:21 -0700

    8. Safe function

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -8,6 +8,6 @@ def safe_function(fct, *args):
     try:
         a = fct(*args)
         return (a)
-    except a.DoesNotExist:
+    except Exception as ex:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
