358e00f9d2f2af2a65a4182a2e1f3f446f4271f3
mostafaateya <mostafaateya@gmail.com>
2023-09-18 18:15:24 -0700

    14. Cities in state

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
-    X = sessionmaker(bind=engine)
-    x = X()
-    for a in (x.query(State.name, City.id, City.name)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in (session.query(State.name, City.id, City.name)
                      .filter(State.id == City.state_id)):
-        print(a[0] + ": (" + str(a[1]) + ") " + a[2])
+        print(instance[0] + ": (" + str(instance[1]) + ") " + instance[2])
