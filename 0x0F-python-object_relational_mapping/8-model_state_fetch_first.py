d0960b4b2f5dad22eed7ea6261cf30544e5fb152
mostafaateya <mostafaateya@gmail.com>
2023-09-18 17:58:11 -0700

    8. First state

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -8,10 +8,10 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    s = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(s)
-    X = sessionmaker(bind=s)
+    Base.metadata.create_all(engine)
+    X = sessionmaker(bind=engine)
     x = X()
     a = x.query(State).first()
     if a is None:
