964fc8c02f0243dfd4db4fd23c45d475b8b0fbd8
mostafaateya <mostafaateya@gmail.com>
2023-09-18 18:12:15 -0700

    14. Cities in state

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" prints the State object with the name passed as argument from the database
+"""
+import sys
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    X = sessionmaker(bind=engine)
+    x = X()
+    for a in (x.query(State.name, City.id, City.name)
+                     .filter(State.id == City.state_id)):
+        print(a[0] + ": (" + str(a[1]) + ") " + a[2])
