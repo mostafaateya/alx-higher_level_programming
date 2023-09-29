be7b9564b8ea1194666829a5502b4e40b7bf445f
mostafaateya <mostafaateya@gmail.com>
2023-09-29 14:17:37 -0700

    8. Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-"""sends a POST request to http://0.0.0.0:5000/search_user
-   with a passed letter as a parameter"""
-from sys import argv
-from requests import post
+'''
+Takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+'''
 
+import requests
+import sys
 
-if __name__ == '__main__':
-    q = ""
-    if len(argv) == 2:
-        q = argv[1]
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_user'
+    arg_length = len(sys.argv)
+    if arg_length == 2:
+        q = sys.argv[1]
+    else:
+        q = ""
+    req = requests.post(url, data={'q': q})
     try:
-        request = post('http://0.0.0.0:5000/search_user', data={'q': q})
-        r_dict = request.json()
-        id = r_dict.get('id')
-        name = r_dict.get('name')
-        if len(r_dict) == 0 or not id or not name:
-            print("No result")
+        req = req.json()
+        if not req:
+            print('No result')
         else:
-            print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
-    except:
-        print("Not a valid JSON")
+            print('[{}] {}'.format(req['id'], req['name']))
+    except ValueError:
+        print('Not a valid JSON')
