46bfa69668754544c40e4cb964da890fdffadb65
mostafaateya <mostafaateya@gmail.com>
2023-09-29 14:41:43 -0700

    4. What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,6 +6,7 @@ import requests
 
 if __name__ == "__main__":
     url = requests.get('https://intranet.hbtn.io/status')
-    print('Body response:')
-    print('\t- type: {}'.format(type(url.text)))
-    print('\t- content: {}'.format(url.text))
+    print("Body response:")
+    print("\t- type: {}".format(type(url.text)))
+    print("\t- content: {}".format(url.text))
+    print("\n")
