3e4f8640ff2c37e1d731e3c9324e2e01946b9daf
mostafaateya <mostafaateya@gmail.com>
2023-09-29 14:03:11 -0700

    9. My GitHub!

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+ takes your GitHub credentials
+ (username and password) and uses the
+ GitHub API to display your id
+ """
+
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    a = (argv[1], argv[2])
+    x = requests.get("https://api.github.com/user", auth=a)
+    print(x.json().get("id"))
