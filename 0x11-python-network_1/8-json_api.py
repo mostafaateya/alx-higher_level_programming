ef776e74e5a71d561d232911c7d8b352e17d6a78
mostafaateya <mostafaateya@gmail.com>
2023-09-29 14:00:31 -0700

    8. Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,12 +9,12 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    x = {"q": argv[1] if len(argv) > 1 else ""}
-    a = requests.post("http://0.0.0.0:5000/search_user", data=x)
+    d = {"q": argv[1] if len(argv) > 1 else ""}
+    request = requests.post("http://0.0.0.0:5000/search_user", data=d)
     try:
-        z = a.json()
-        if z:
-            print("[{}] {}".format(z.get("id"), z.get("name")))
+        json = request.json()
+        if json:
+            print("[{}] {}".format(json.get("id"), json.get("name")))
         else:
             print("No result")
     except:
