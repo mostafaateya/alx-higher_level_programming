758dcc81252bcec69435ce0b0ad4d70d7bd76bc6
mostafaateya <mostafaateya@gmail.com>
2023-09-29 13:58:06 -0700

    8. Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with
+the letter as a parameter.
+"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    x = {"q": argv[1] if len(argv) > 1 else ""}
+    a = requests.post("http://0.0.0.0:5000/search_user", data=x)
+    try:
+        z = a.json()
+        if z:
+            print("[{}] {}".format(z.get("id"), z.get("name")))
+        else:
+            print("No result")
+    except:
+        print("Not a valid JSON")
